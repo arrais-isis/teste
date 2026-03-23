67cb54898a9511ed77f5cce0de97ddfc2eac59f0
Isis <isisarrais15@gmail.com>
2026-03-23 09:54:25 -0300

    inicio banco de dados

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Teste")
+import mysql.connector
+
+banco = mysql.connector.connect(
+    host="10.30.29.162",
+    port=3309,
+    user="root",
+    password="root123",
+    database="isis"
+)
+
+cursor = banco.cursor()
+
+# cursor.execute("CREATE TABLE teste (ID INT PRIMARY KEY, NOME TEXT);")
+cursor.execute("INSERT INTO teste VALUES (1, 'vinicius')")
+banco.commit() # Usado em caso de alteração nos dados (insert, delete, update)
+
